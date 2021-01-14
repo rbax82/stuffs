87807b84b5066e5d7160dff911f2f1b0ad47eace
Robert Baxter <rbax82@users.noreply.github.com>
2021-01-14 12:15:46 -0700

    Update gogogadgetburnserver.sh
    fuck nouveau driver, it's trash

--- gogogadgetburnserver.sh
+++ gogogadgetburnserver.sh
@@ -1,5 +1,9 @@
 apt install snmpd lm-sensors apt-listchanges git build-essential cmake libuv1-dev libssl-dev libhwloc-dev htop vim net-tools
 echo "Debian-snmp ALL = NOPASSWD: /usr/sbin/dmidecode" > /etc/sudoers.d/snmp
 echo "snmp ALL = NOPASSWD: /usr/sbin/dmidecode" >> /etc/sudoers.d/snmp
+echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+update-initramfs -u
 
 for f in /sys/devices/system/cpu/vulnerabilities/*; do echo "${f##*/} -" $(cat "$f"); done
+
